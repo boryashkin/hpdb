3f1eefa46163f5cc663c07b97af6e8b9f1144b60
borisd <mail@borisd.ru>
2018-12-16 03:16:52 +0500

    New design and real profile page

--- src/actions/web/Profile.php
+++ src/actions/web/Profile.php
@@ -2,17 +2,26 @@
 namespace app\actions\web;
 
 use app\abstracts\BaseAction;
+use app\modules\web\ProfileRepository;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\NotFoundException;
 
 class Profile extends BaseAction
 {
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
+        $profileId = $request->getAttribute('id');
+        $repo = new ProfileRepository();
+        $profile = $repo->getOne($profileId);
+        if (!$profile) {
+            throw new NotFoundException($request, $response);
+        }
+
         return $this->getView()->render($response, 'web/profile.html', [
-            'url' => 'https://borisd.ru',
-            'title' => 'Десятский Борис',
-            'description' => 'Борющийся с ленью',
+            'url' => $profile['homepage'],
+            'title' => $profile['homepage'],
+            'description' => 'Нет описания',
         ]);
     }
 }
