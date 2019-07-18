67ae24434f77a835de93dfd7baba77f0dcda4d2e
borisd <mail@borisd.ru>
2019-07-18 22:45:44 +0500

    Main: "new" block - 5 last added websites

--- src/actions/web/Index.php
+++ src/actions/web/Index.php
@@ -4,6 +4,7 @@ namespace app\actions\web;
 use app\abstracts\BaseAction;
 use app\models\Website;
 use app\models\WebsiteContent;
+use Jenssegers\Mongodb\Collection;
 use MongoDB\BSON\ObjectId;
 use MongoDB\Collection as MongoCollection;
 use Psr\Http\Message\ResponseInterface;
@@ -18,6 +19,18 @@ class Index extends BaseAction
          * @var MongoCollection $c
          */
         $c = $mongo->getCollection('websiteReaction');
+        $c = $mongo->getCollection('websiteReaction');
+        $reactions = $this->getTopReactions($c);
+        $newWebsites = $this->getNewWebsites($c);
+
+        return $this->getView()->render($response, 'web/index.html', [
+            'reactions' => $reactions,
+            'newWebsites' => $newWebsites,
+        ]);
+    }
+
+    private function getTopReactions(Collection $c)
+    {
         $reactions = $c->aggregate([
             //todo: filter out ip duplicates
             [
@@ -50,8 +63,39 @@ class Index extends BaseAction
             ];
         }
 
-        return $this->getView()->render($response, 'web/index.html', [
-            'reactions' => $result,
-        ]);
+        return $result;
+    }
+
+    private function getNewWebsites(Collection $c)
+    {
+        $websites = Website::query()->orderBy('created_at', 'desc')->limit(5)->get();
+
+        $result = [];
+        foreach ($websites as $website) {
+            $reactions = $c->aggregate([
+                [
+                    '$match' => ['website_id' => new ObjectId($website->_id)],
+                ],
+                [
+                    '$group' => [
+                        '_id' => ['website_id' => '$website_id', 'reaction' => '$reaction'],
+                        'count' => ['$sum' => 1],
+                    ]
+                ],
+            ]);
+            //damn you laravel
+            $content = WebsiteContent::query()
+                ->where('website_id', '=', new ObjectId($website->_id))
+                ->orderBy('created_at', -1)->first();
+
+            $result[] = [
+                'profile_id' => $website->profile_id,
+                'homepage' => $website->homepage,
+                'reactions' => $reactions,
+                'title' => $content->title ? \substr(trim($content->title ?? ''), 0, 50) : 'No description yet',
+            ];
+        }
+
+        return $result;
     }
 }
