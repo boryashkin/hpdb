3dc5dfa3cca7200d892cc0dabe9b7f426263de8f
borisd <mail@borisd.ru>
2020-03-08 13:58:20 +0300

    WebsiteRepository: fix getOneByHomepage condition

--- src/messageBus/repositories/WebsiteRepository.php
+++ src/messageBus/repositories/WebsiteRepository.php
@@ -30,7 +30,7 @@ class WebsiteRepository extends AbstractMongoRepository
     public function getOneByHomepage(string $url)
     {
         return Website::query()
-            ->where('_id', '=', $url)
+            ->where('homepage', '=', $url)
             ->limit(1)
             ->first();
     }
